641b77edb7e952e3ac792920a38684304951a46b
Jiří Kofránek <kofranek@gmail.com>
2021-09-27 19:06:03 +0200

    added useState

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './counter';
 
 const Counters = () => {
+  const [counters, setCounters] = useState([{ value: 0, id: 1 }, { value: 0, id: 2 }, { value: 0, id: 3 }])
   return (
     <div>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      { counters.map(counter => <Counter key={ counter.count }/>) }
     </div>
   );
 };
