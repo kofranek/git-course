650be0788a97bda2c89bb158c93a721ee6d96dda
Jiří Kofránek <kofranek@gmail.com>
2021-10-01 00:52:14 +0200

    nefungující Reset

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,30 +2,36 @@ import React, { useState } from 'react';
 import Counter from './counter';
 
 const Counters = () => {
-  const [counters, setCounters] = useState(
-    [
-      { value: 0, id: 1, name: 'Ложка', nameCzech: 'Lžička'},
-      { value: 5, id: 2, name: 'Вилка', nameCzech: 'Vidlička'},
-      { value: 0, id: 3, name: 'Тарелка', nameCzech: 'Talíř' },
-      { value: 0, id: 4, name: 'Стартовый набор минималиста', nameCzech: 'Minimalistická startovací sada' },
-      { value: 0, id: 5, name: 'Ненужные вещи', nameCzech: 'Nepotřebné věci' },
-    ])
-  const handleDelete =(counterId)=>{
-    console.log('handleDelete id=',counterId)
-    const newCounters=counters.filter((el)=>el.id!==counterId)
+  const initialState = [
+    { value: 0, id: 1, name: 'Ложка', nameCzech: 'Lžička' },
+    { value: 5, id: 2, name: 'Вилка', nameCzech: 'Vidlička' },
+    { value: 0, id: 3, name: 'Тарелка', nameCzech: 'Talíř' },
+    { value: 0, id: 4, name: 'Стартовый набор минималиста', nameCzech: 'Minimalistická startovací sada' },
+    { value: 0, id: 5, name: 'Ненужные вещи', nameCzech: 'Nepotřebné věci' },
+  ]
+  const [counters, setCounters] = useState(initialState)
+  const handleReset = () => {
+    console.log('volám setCounters(initialState)')
+    setCounters(initialState)
+  }
+
+  const handleDelete = (counterId) => {
+    console.log('handleDelete id=', counterId)
+    const newCounters = counters.filter((el) => el.id !== counterId)
     setCounters(newCounters)
   }
   return (
     <div>
+      <button onClick={ handleReset } className="btn btn-primary btn-sm m-2">Reset</button>
       { counters.map(counter => (
         <Counter key={ counter.id }
-                 /*
-                 id = {counter.id}  //key is not a props
-                 value={ counter.value }
-                 name={ counter.name }
-                  */
-                 {...counter} //spread operator - allows you to add additional parameters
-                 onDelete={handleDelete}
+          /*
+          id = {counter.id}  //key is not a props
+          value={ counter.value }
+          name={ counter.name }
+           */
+                 { ...counter } //spread operator - allows you to add additional parameters
+                 onDelete={ handleDelete }
         >
         </Counter>
       )) }
