082f559b27a35c1e9fd2d71e35dc1ce7798d118a
Jiří Kofránek <kofranek@gmail.com>
2021-10-02 22:33:18 +0200

    pozvednutí stavu do vrcholu stromu komponent - nyní vše se řídí z hlavní rosičovaké komponenty App props ve volání nahrazeno destrukturalizací, např.: Counters = ({onReset, counters, onDelete, onIncrement, onDecrement})

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import Counter from './counter';
 
-const Counters = (props) => {
+const Counters = ({onReset, counters, onDelete, onIncrement, onDecrement}) => {
 
   return (
     <div>
-      <button onClick={ props.onReset } className="btn btn-primary btn-sm m-2">Reset</button>
-      { props.counters.map(el => (
+      <button onClick={ onReset } className="btn btn-primary btn-sm m-2">Reset</button>
+      { counters.map(el => (
         <Counter key={ el.id }
                  { ...el } //spread operator - allows you to add additional parameters
-                 onDelete={ props.onDelete }
-                 onIncrement={props.onIncrement}
-                 onDecrement={props.onDecrement}
+                 onDelete={ onDelete }
+                 onIncrement={onIncrement}
+                 onDecrement={onDecrement}
         >
         </Counter>
       )) }
