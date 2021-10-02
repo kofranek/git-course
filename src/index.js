1f069811dd366b5ca05dda8b618a7d8e18bafd63
Jiří Kofránek <kofranek@gmail.com>
2021-10-02 04:54:37 +0200

    SYnchronizace několika komponentů

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import 'bootstrap/dist/css/bootstrap.css'
-import Counters from './components/counters'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counters from './components/counters';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Counters />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+   document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
