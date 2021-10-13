8d40849ec3e27b32d1c356f42a3942d3e27e851b
Jiří Kofránek <kofranek@gmail.com>
2021-10-13 17:27:57 +0200

    destrukturalizace App, Counter, Counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import Counter from './counter';
 
-const Counters = (props) => {
-    console.log('Counters props=',props)
+const Counters = ({onReset, counters, ...rest }) => {
+    console.log('Counters counters=',counters,'onReset=',onReset, 'rest=',rest)
   return (
     <div>
       <button
-        onClick={ props.onReset }
+        onClick={ onReset }
         className="btn btn-primary btn-sm m-2">
         Reset
       </button>
-      { props.counters.map((counter, index) => (
+      { counters.map((counter, index) => (
         <Counter
           key={counter.id}
-          id={counter.id}
-          value={counter.value}
-          name={counter.name}
-          nameCzech={counter.nameCzech}
-          onDecrement={props.onDecrement}
-          onIncrement={props.onIncrement}
-          onDelete={props.onDelete}
+          // id={counter.id}
+          // value={counter.value}
+          // name={counter.name}
+          // nameCzech={counter.nameCzech}
+          // onDecrement={props.onDecrement}
+          // onIncrement={props.onIncrement}
+          // onDelete={props.onDelete}
+          {...counter}
+          {...rest}
+
         >
         </Counter>
       )) }
